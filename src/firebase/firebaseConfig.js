fee53e2acd43f9126b58ceacbe5a2c40c99e5e07
ChrisHo <christopher.ho@ihis.com.sg>
2022-11-11 07:50:44 +0800

    feat: Added firestore integration, added loading spinner

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,33 +1,35 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 /**
  * It initializes Firebase with the configuration values that are stored in the .env file.
  */
-export const initializeFirebase = () => {
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    // @ts-ignore
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    // @ts-ignore
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    // @ts-ignore
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    // @ts-ignore
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCK,
-    // @ts-ignore
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGE_SENDER_ID,
-    // @ts-ignore
-    appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    // @ts-ignore
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  getAnalytics(app);
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  // @ts-ignore
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  // @ts-ignore
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  // @ts-ignore
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  // @ts-ignore
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCK,
+  // @ts-ignore
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGE_SENDER_ID,
+  // @ts-ignore
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  // @ts-ignore
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const analytics = getAnalytics(app);
+export default getFirestore(app);
--- src/firebase/logFirebaseEvent.js
+++ src/firebase/logFirebaseEvent.js
@@ -1,12 +1,11 @@
-import { getAnalytics, logEvent } from "firebase/analytics";
-
+import { logEvent } from "firebase/analytics";
+import { analytics } from "./firebaseConfig";
 /**
  * It takes an event object and a song name, and logs the event to Firebase Analytics
  * @param event - The event object that we created in the previous step.
  * @param songName - The name of the song that was played.
  */
 export const logFirebaseEvent = (event, songName) => {
-  const analytics = getAnalytics();
   logEvent(analytics, event.eventName, {
     ...event.params,
     songName: songName ?? "",
