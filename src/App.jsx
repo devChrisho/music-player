b05dcc0d81d7a31b3a8fc3fe564584bcb4841504
ChrisHo <christopher.ho@ihis.com.sg>
2022-11-11 07:50:44 +0800

    feat: Added firestore integration, added loading spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./styles/app.scss";
 import data from "./data";
 import Library from "./components/Library";
@@ -7,14 +7,12 @@ import Player from "./components/Player";
 import Song from "./components/Song";
 import EVENTS from "./firebase/events";
 import { logFirebaseEvent } from "./firebase/logFirebaseEvent";
+import { collection, onSnapshot } from "firebase/firestore";
+import db from "./firebase/firebaseConfig";
+import { Bars } from "react-loader-spinner";
 
 function App({ isDarkTheme, setIsDarkTheme }) {
-  // log to firebase tracker when app loads
-  useEffect(() => {
-    logFirebaseEvent(EVENTS.VIEW.LANDING, "");
-  }, []);
-
-  // !var States
+  const [isLoading, setIsLoading] = useState(false);
   const [songs, setSongs] = React.useState(data());
   const [currentSong, setCurrentSong] = React.useState(songs[0]);
   const [isPlaying, setIsPlaying] = React.useState(false);
@@ -25,26 +23,7 @@ function App({ isDarkTheme, setIsDarkTheme }) {
   });
   const [libraryStatus, setLibraryStatus] = React.useState(false);
 
-  // !exp Ref
   const audioRef = React.useRef(null);
-
-  const timeUpdateHandler = (e) => {
-    // !exp currentTime and duration are audio element built in attributes (https://developer.mozilla.org/en-US/docs/Web/HTML/Element/audio)
-    const currentTime = e.target.currentTime;
-    const duration = e.target.duration;
-
-    // calculate percentage
-    const roundedCurrent = Math.round(currentTime);
-    const roundedDuration = Math.round(duration);
-    const animation = Math.round((roundedCurrent / roundedDuration) * 100);
-
-    setSongInfo({
-      ...songInfo,
-      currentTime,
-      duration,
-      animationPercentage: animation,
-    });
-  };
 
   const songEndHandler = async () => {
     let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
@@ -53,48 +32,107 @@ function App({ isDarkTheme, setIsDarkTheme }) {
     if (isPlaying) audioRef?.current?.play();
   };
 
+  const collectionRef = collection(db, "songs");
+
+  // log to firebase tracker when app loads
+  useEffect(() => {
+    logFirebaseEvent(EVENTS.VIEW.LANDING, "");
+  }, []);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const unsub = onSnapshot(collectionRef, (querySnapShot) => {
+      const items = [];
+      querySnapShot.forEach((doc) => {
+        items.push({ ...doc.data(), id: doc.id });
+      });
+
+      setSongs(items);
+      setIsLoading(false);
+    });
+
+    return () => {
+      unsub();
+    };
+  }, []);
+
   return (
     <div
       className={`App ${libraryStatus ? "library-active" : ""}`}
       id={isDarkTheme ? "darkMode" : "lightMode"}>
-      <Nav
-        libraryStatus={libraryStatus}
-        setLibraryStatus={setLibraryStatus}
-        setIsDarkTheme={setIsDarkTheme}
-        isDarkTheme={isDarkTheme}
-      />
-      <Song currentSong={currentSong} isPlaying={isPlaying} />
-      <Player
-        currentSong={currentSong}
-        isPlaying={isPlaying}
-        setIsPlaying={setIsPlaying}
-        audioRef={audioRef}
-        songInfo={songInfo}
-        setSongInfo={setSongInfo}
-        songs={songs}
-        setCurrentSong={setCurrentSong}
-        setSongs={setSongs}
-      />
-      <Library
-        songs={songs}
-        setCurrentSong={setCurrentSong}
-        audioRef={audioRef}
-        isPlaying={isPlaying}
-        setSongs={setSongs}
-        libraryStatus={libraryStatus}
-      />
-      <audio
-        //this event occurs when the time is updated
-        onTimeUpdate={timeUpdateHandler}
-        //this event occurs when meta data is loaded
-        onLoadedMetadata={timeUpdateHandler}
-        //this passes this element as audioRef like document.querySelector
-        ref={audioRef}
-        src={currentSong.audio}
-        onEnded={songEndHandler}
-      />
+      {!isLoading ? (
+        <>
+          <Nav
+            libraryStatus={libraryStatus}
+            setLibraryStatus={setLibraryStatus}
+            setIsDarkTheme={setIsDarkTheme}
+            isDarkTheme={isDarkTheme}
+          />
+          <Song currentSong={currentSong} isPlaying={isPlaying} />
+          <Player
+            currentSong={currentSong}
+            isPlaying={isPlaying}
+            setIsPlaying={setIsPlaying}
+            audioRef={audioRef}
+            songInfo={songInfo}
+            setSongInfo={setSongInfo}
+            songs={songs}
+            setCurrentSong={setCurrentSong}
+            setSongs={setSongs}
+          />
+          <Library
+            songs={songs}
+            setCurrentSong={setCurrentSong}
+            audioRef={audioRef}
+            isPlaying={isPlaying}
+            setSongs={setSongs}
+            libraryStatus={libraryStatus}
+          />
+          <audio
+            //this event occurs when the time is updated
+            onTimeUpdate={(event) =>
+              timeUpdateHandler(event, songInfo, setSongInfo)
+            }
+            //this event occurs when meta data is loaded
+            onLoadedMetadata={(event) =>
+              timeUpdateHandler(event, songInfo, setSongInfo)
+            }
+            //this passes this element as audioRef like document.querySelector
+            ref={audioRef}
+            src={currentSong.audio}
+            onEnded={songEndHandler}
+          />
+        </>
+      ) : (
+        <div
+          style={{
+            margin: "0 auto",
+            marginTop: "50%",
+          }}>
+          <Bars color={isDarkTheme ? "#81789b" : "#606060"} />
+        </div>
+      )}
     </div>
   );
 }
 
 export default App;
+
+// Helper function
+const timeUpdateHandler = (event, songInfo, setSongInfo) => {
+  // !exp currentTime and duration are audio element built in attributes (https://developer.mozilla.org/en-US/docs/Web/HTML/Element/audio)
+  const currentTime = event.target.currentTime;
+  const duration = event.target.duration;
+
+  // calculate percentage
+  const roundedCurrent = Math.round(currentTime);
+  const roundedDuration = Math.round(duration);
+  const animation = Math.round((roundedCurrent / roundedDuration) * 100);
+
+  setSongInfo({
+    ...songInfo,
+    currentTime,
+    duration,
+    animationPercentage: animation,
+  });
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import AppWrapper from "./AppWrapper";
-import { initializeFirebase } from "./firebase/firebaseConfig";
-
-// initialize Firebase
-initializeFirebase();
 
 const container = document.getElementById("root");
 // @ts-ignore
