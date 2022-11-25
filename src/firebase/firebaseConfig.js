8f66a9f81ac1a1dcdb6ed19d94d3ef4b09b19940
ChrisHo <68413782+superchrisho@users.noreply.github.com>
2022-11-25 13:35:26 +0800

    Refactored code

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,18 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
-import "firebase/auth";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 /**
  * It initializes Firebase with the configuration values that are stored in the .env file.
  */
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   // @ts-ignore
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   // @ts-ignore
@@ -28,12 +19,3 @@ const firebaseConfig = {
   // @ts-ignore
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
-
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-
-// Initialize firestore
-export const db = getFirestore(app);
-
-// Initialize analytics
-export const analytics = getAnalytics(app);
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,6 +1,20 @@
-import { logFirebaseEvent } from "./logFirebaseEvent";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { firebaseConfig } from "./firebaseConfig";
 import EVENTS from "./events";
-import { app, db, analytics } from "./firebaseConfig";
+import { logFirebaseEvent } from "./logFirebaseEvent";
+import "firebase/auth";
 
-export { EVENTS, logFirebaseEvent, db, analytics };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = getFirestore(app);
+
+// Initialize Analytics
+const analytics = getAnalytics(app);
+
+export { EVENTS, db, analytics, logFirebaseEvent };
 export default app;
